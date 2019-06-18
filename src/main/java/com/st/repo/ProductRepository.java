be6648c9f049899d2f6228aef34b7952aabd01a2
admin <admin@192.168.0.6>
2019-06-19 00:44:14 +0530

    commit

--- src/main/java/com/st/repo/ProductRepository.java
+++ src/main/java/com/st/repo/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.st.repo;
 
 
+
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.st.entity.Product;
@@ -12,6 +17,30 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	
 	@Query("select p from com.st.entity.Product p")
 	public List<Product> getInfo();
+	@Query("select p.prodCost from com.st.entity.Product p")
+	public List<Double> getCostOnly();
+	@Query("select p.prodId,p.prodName from com.st.entity.Product p")
+	public List<Object[]> getData();
+	@Query("select p from com.st.entity.Product p  where p.prodId=?1 or p.prodCost<?2")
+	public List<Product> getNewData(Integer pid,Double pcost);
+	@Query("select p from com.st.entity.Product p where p.prodName=:name or p.prodModel=:model")
+	public List<Product> getNamedData(String name,String model);
+	@Query("select p from Product p")
+	public List<Product> withoutPack();
+	//@Query("update Product p set p.prodCost=?1 where p.prodId=?2")
+	@Transactional @Modifying
+	@Query("update Product p set p.prodCost=:cost where p.prodId=:id")
+	public int getUpdate(double cost, int id);
+	
+	@Transactional @Modifying
+	@Query("delete  from Product p where p.prodId=:id")
+	public int remove(int id);
+	//bad approach this one
+	@Query("select p from  Product p where p.prodId=:pid")
+	Product getOneRecds(int pid);
+	
+	@Query("select p from  Product p where p.prodId=:pid")
+	Optional<Product> getOneRecds1(int pid);
 }
 
 
--- src/main/java/com/st/runner/ConsoleRunner.java
+++ src/main/java/com/st/runner/ConsoleRunner.java
@@ -1,10 +1,13 @@
 package com.st.runner;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.st.entity.Product;
 import com.st.repo.ProductRepository;
 
 @Component
@@ -14,6 +17,31 @@ public class ConsoleRunner implements CommandLineRunner {
 private ProductRepository repo;
 	@Override
 	public void run(String... args) throws Exception {
-			repo.getInfo().forEach(System.out::println);
+		/*
+		 * repo.getInfo().forEach(System.out::println);
+		 * repo.getCostOnly().forEach(s->System.out.println("Cost only: "+s));
+		 * repo.getData().forEach(s->System.out.println(s[0]+"......"+s[1]));
+		 * repo.getNewData(101,6.5).forEach(System.out::println);
+		 * repo.getNamedData("Spring", "database").forEach(System.out::println);
+		 * repo.withoutPack().forEach(System.out::println);
+		 */		
+		/*
+		 * int update=repo.getUpdate(12.5, 111);
+		 * System.out.println(update+": row is updated");
+		 */
+		/*
+		 * int update =repo.remove(111); System.err.println(update);
+		 */
+		
+		Optional<Product> p=repo.getOneRecds1(106);
+		if (p.isPresent()) {
+			Product p1=p.get();
+			System.err.println(p1.getProdName()+"\t"+p1.getProdModel());
+		} else {
+			System.err.println("DATA IS Found........");
+		}
+		
+	
+			
 	}
 }
